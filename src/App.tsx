0e0549fa668d381d9b27c3d6c136c4578ad6018a
GearHead87 <hosanulislam87@gmail.com>
2024-07-03 17:58:24 +0600

    Project setup with Vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HomePage from "./Page/Home/HomePage";
+
+function App() {
+	return (
+		<div className="container mx-auto border-2">
+			<HomePage></HomePage>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Page/Home/HomePage.tsx
@@ -0,0 +1,11 @@
+import NavBar from '../../components/shared/NavBar';
+
+const HomePage = () => {
+	return (
+		<div>
+			<NavBar />
+		</div>
+	);
+};
+
+export default HomePage;
